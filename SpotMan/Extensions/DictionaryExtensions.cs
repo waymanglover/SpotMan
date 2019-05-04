ae79ae80ff7e429ae8c980b5bd93528700aea8c2
Wayman Glover <waymanglover@gmail.com>
2019-05-04 12:04:27 -0400

    Add Extension methods for dictionary -> URL parameter convertion.

---
+++ SpotMan/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SpotMan.Extensions
+{
+    public static class DictionaryExtensions
+    {
+        public static string ToUrlParamString(this Dictionary<string, string> urlParams)
+        {
+            return '?' + string.Join('&',
+                       urlParams.Select(p =>
+                           HttpUtility.UrlEncode(p.Key) + '=' + HttpUtility.UrlEncode(p.Value)
+                       )
+                   );
+        }
+    }
+}
