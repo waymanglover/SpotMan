4ffe15fe61357b129782e69919197350565abe3b
Wayman Glover <waymanglover@gmail.com>
2019-05-04 12:00:02 -0400

    Additional URL constants

--- SpotMan/Helpers/Constants.cs
+++ SpotMan/Helpers/Constants.cs
@@ -2,7 +2,10 @@
 {
     internal static class Constants
     {
-        public const string UrlSpotifyAccountAuthorize = "https://accounts.spotify.com/authorize";
+        public const string UrlSpotifyAuthorize = "https://accounts.spotify.com/authorize";
+        public const string UrlSpotifyToken = "https://accounts.spotify.com/api/token";
         public const string UrlSpotifyApi = "https://api.spotify.com/v1";
+
+        public const string LocalUrlAuthCallback = "api/UserAuth/callback";
     }
 }
