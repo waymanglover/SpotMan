02763782453004b41f1829151e96da3a6dc408c6
Wayman Glover <waymanglover@gmail.com>
2019-05-05 06:43:04 -0400

    Stub out Spotify controller.

---
+++ SpotMan/Controllers/SpotifyController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using SpotMan.Helpers;
+using SpotMan.Models;
+
+namespace SpotMan.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SpotifyController : SpotManControllerBase
+    {
+        private HttpClient HttpClient { get; }
+
+        public SpotifyController(IConfiguration configuration)
+        {
+            HttpClient = new HttpClient()
+            {
+                BaseAddress = new Uri(ConfigurationHelper.SelfUrl),
+                Timeout = TimeSpan.FromSeconds(ConfigurationHelper.TimeoutSeconds),
+            };
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status302Found)]
+        public ActionResult<User> GetCurrentUser()
+        {
+            return Success();
+        }
+    }
+}
