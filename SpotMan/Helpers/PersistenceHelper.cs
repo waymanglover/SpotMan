c7e2559eb54b93b51681577880bd61c28cde534f
Wayman Glover <waymanglover@gmail.com>
2019-05-05 04:05:19 -0400

    Add functions for getting persisted keys, and fix typo in filename

--- SpotMan/Helpers/PersistenceHelper.cs
+++ SpotMan/Helpers/PersistenceHelper.cs
@@ -5,12 +5,38 @@ using Microsoft.Win32;
 
 namespace SpotMan.Helpers
 {
+    // TODO: Interface with separate implementations?
     public static class PersistenceHelper
     {
         private const string RegistryLocation = @"SOFTWARE\SpotMan";
+        public static Dictionary<string, string> GetKeys()
+        {
+            // TODO: Azure key vault?
+            return RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? GetKeysWindows() : GetKeysJson();
+        }
+
+        private static Dictionary<string, string> GetKeysJson()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private static Dictionary<string, string> GetKeysWindows()
+        {
+            var subKey = Registry.CurrentUser.OpenSubKey(RegistryLocation);
+            var output = new Dictionary<string, string>();
+            if (subKey == null) return output;
+
+            var names = subKey.GetValueNames();
+            foreach (var name in names)
+            {
+                output.Add(name, subKey.GetValue(name).ToString());
+            }
+            return output;
+        }
 
         public static void StoreKeys(Dictionary<string, string> keys)
         {
+            // TODO: Azure key vault?
             if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
             {
                 StoreKeysWindows(keys);
