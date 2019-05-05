27cc61fb62facaa87d856232eb6c0ed72ec5f696
Wayman Glover <waymanglover@gmail.com>
2019-05-05 19:00:59 -0400

    Add model representing OAuth token information.

---
+++ SpotMan/Models/OAuthToken.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using SpotMan.Helpers;
+
+namespace SpotMan.Models
+{
+    public class OAuthToken
+    {
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+        public DateTimeOffset Expiry { get; set; }
+
+        public OAuthToken()
+        {
+            var spotManRegistry = PersistenceHelper.GetKeys();
+            AccessToken = spotManRegistry[nameof(AccessToken)];
+            RefreshToken = spotManRegistry[nameof(RefreshToken)];
+            Expiry = DateTimeOffset.Parse(spotManRegistry[nameof(Expiry)]);
+        }
+    }
+}
